8103227a253d13394db83820e2693cffcb8d77dd
Matthias Covi (Wifi Festplatte) <m.covi@cwp.uno>
2023-02-02 21:28:01 +0100

    Business und Gui angefangen

---
+++ Swd.PlayCollector/Swd.PlayCollector.Business/CollectionItemService.cs
@@ -0,0 +1,22 @@
+﻿using Swd.PlayCollector.Model;
+using Swd.PlayCollector.Repository;
+
+namespace Swd.PlayCollector.Business
+{
+    public class CollectionItemService
+    {
+        private ICollectionItemRepository _IRepository;
+
+        public CollectionItemService()
+        {
+            _IRepository = new CollectionItemRepository();
+        }
+
+        public async Task<IQueryable<CollectionItem>> GetAllAsync()
+        {
+            
+            var resultList = await _IRepository.GetAllAsync();
+            return resultList;
+        }
+    }
+}
